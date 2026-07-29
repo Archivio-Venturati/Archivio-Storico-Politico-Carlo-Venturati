a318a3ceb2affa70788fee9fe6fd619c9545ac30
mirkogatti1998 <mirko.gatti1998@gmail.com>
2026-07-29 19:41:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -762,7 +762,17 @@ function render() {
 
 
 function wireEvents() {
-  window.addEventListener("hashchange", render);
+ window.addEventListener("hashchange", () => {
+  render();
+
+  requestAnimationFrame(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant"
+    });
+  });
+});
 
   const goArchivio = () => {
     const h = location.hash || "#/";
